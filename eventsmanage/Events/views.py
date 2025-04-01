86d8d9af1a380de3693c68513dd2cc4fab89dd2d
alexander784 <alexandernyaga1999@gmail.com>
2025-04-01 11:26:19 +0300

    Create event func

--- eventsmanage/Events/views.py
+++ eventsmanage/Events/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from models import Event,Category
 
 # Create your views here.
+
+def create_event(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        category_id = request.POST.get('category')
+        start_date = request.POST.get('start_date')
+        end_date = request.POST.get('end_date')
+        priority = request.POST.get('priority')
+        description = request.POST.get('description')
+        location = request.POST.get('location')
+        organizer = request.POST.get('organizer')
+
+        # Retrieve the Category object
+        category = Category.objects.get(pk=category_id)
+        event = Event.objects.create(
+            name=name,
+            category=category,
+            start_date=start_date,
+            end_date=end_date,
+            priority=priority,
+            description=description,
+            location=location,
+            organizer=organizer
+        )
+        return redirect('category_list')
+    else:
+        categories = Category.objects.all()
+        return render(request, 'event_management_system_app/create_event.html', {'categories': categories})
