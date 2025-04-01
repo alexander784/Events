ada65a67a6c06e17bd52a6234c98ddab0e9fce1d
alexander784 <alexandernyaga1999@gmail.com>
2025-04-01 11:22:17 +0300

    Build the models

--- eventsmanage/Events/models.py
+++ eventsmanage/Events/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+class Event(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    priority = models.IntegerField(default=1)
+    description = models.TextField(default='')
+    location = models.CharField(max_length=255, default='')
+    organizer = models.CharField(max_length=100, default='')
+
